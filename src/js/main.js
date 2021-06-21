c921e1950df44bf24cca327220451c036a067397
wegiveyoubrand <wegiveyoubrand@gmail.com>
2021-06-21 21:57:47 +0100

    Updated

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+// Select DOM items
+const menuBtn = document.querySelector('.menu-toggle');
+const my_account = document.querySelector('.my_account');
+const menu = document.querySelector('.aside')
+const menuItems = document.querySelector('aside')
+// const close_modal = document.querySelector('');
+// const close_menu_toggle = document.querySelector('');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+
+function toggleMenu() {
+    if(!showMenu) {
+        menu.classList.add('open');
+        menuItems.classList.add('show');
+
+        showMenu = true;
+    } else {
+        menu.classList.remove('open');
+        menuItems.classList.remove('show');
+
+        showMenu = false;
+    }
+}
